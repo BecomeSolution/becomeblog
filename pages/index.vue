d4255535580e4f498eb7fb33f841d01c4b1920a4
BecomeSolution <96214283+BecomeSolution@users.noreply.github.com>
2021-12-16 04:00:42 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,7 @@
       </button>
     </site-hero>
     <main-section theme="one-column">
+<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/73235fe20dc99c72d2696df94/db41f1a94ce25ebf4764d9a7a.js");</script>
       <template v-slot:default>
         <!-- All Posts -->
         <posts-grid />
